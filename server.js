051666d515c32696523ff5002db5b33684b9ee21
niaRamaro <ramarosandynia@gmail.com>
2019-12-28 09:35:53 +0300

    Add timestamp API endpoint

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,10 +21,26 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
+app.get("/api/timestamp/:date_string?", function (req, res) {
+  const dateParam = req.params.date_string;
 
+  if (dateParam) {
+    const date = /^[0-9]+$/.test(dateParam) ? new Date(parseInt(dateParam)) : new Date(dateParam);
+    res.json(getResponse(date));
+  } else {
+    res.json(getResponse(new Date()));
+  }
+
+  function getResponse(date) {
+    return {
+      unix: !isNaN(date.getTime()) ? date.getTime() : null,
+      utc: date.toUTCString()
+    };
+  }
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
